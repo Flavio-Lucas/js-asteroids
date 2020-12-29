b8e507562715bf2d089df44f1e62d66b4600bdb7
Flavio-Lucas <flaviolucas389@gmail.com>
2020-12-29 15:48:47 -0300

    feat(asteroids): added asteroids and make their move across the screen

--- asteroids.js
+++ asteroids.js
@@ -1,7 +1,17 @@
 // frames per second
 const FPS = 30;
-// friction coefficient of space (0 = no friction and 1 = lot of friction)
+// friction coefficient of space (0 = none, 1 = lots)
 const FRICTION = .7;
+// jaggednes of the asteroids (0 = none, 1 = lots)
+const ROIDS_JAG = 0.5;
+// starting number of asteroids
+const ROIDS_NUM = 10;
+// starting size of asteroids in pixels
+const ROIDS_SIZE = 100;
+// max starting speed of asteroids in pixels per second
+const ROIDS_SPD = 50;
+// average number of vertices on each asteroid
+const ROIDS_VERT = 10;
 // ship height in pixels
 const SHIP_SIZE = 30;
 // acceleration of the ship in pixels per second
@@ -33,6 +43,11 @@ let ship = {
   }
 }
 
+//set up asteroids
+let roids = [];
+
+createAsteroidBelt();
+
 // set up event handlers
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
@@ -40,7 +55,23 @@ document.addEventListener('keyup', keyUp);
 // set up the game loop
 setInterval(update, 1000 / FPS);
 
-function keyDown(/** @type {KeyboardEvent}*/ e){
+function createAsteroidBelt () {
+  roids = [];
+  let x, y;
+  for(var i = 0; i < ROIDS_NUM; i ++){
+    do {
+      x = Math.floor(Math.random() * canva.width);
+      y = Math.floor(Math.random() * canva.height);
+    } while (distBetweenPoints(ship.x, ship.y, x, y) < ROIDS_SIZE * 2 + ship.r);
+    roids.push(newAsteroid(x, y));
+  }
+}
+
+function distBetweenPoints(shipX, shipY, roidX, roidY){
+  return Math.sqrt(Math.pow(shipX - roidX, 2) + Math.pow(shipY - roidY, 2))
+}
+
+function keyDown (/** @type {KeyboardEvent}*/ e){
   switch(e.keyCode) {
     case 37: // left arrow (rotate ship to left)
       ship.rot = TURN_SPEED / 180 * Math.PI / FPS;
@@ -55,7 +86,7 @@ function keyDown(/** @type {KeyboardEvent}*/ e){
   }
 }
 
-function keyUp(/** @type {KeyboardEvent}*/ e){
+function keyUp (/** @type {KeyboardEvent}*/ e){
   switch(e.keyCode) {
     case 37: // left arrow (stop rotating the ship)
       ship.rot = 0;
@@ -68,6 +99,33 @@ function keyUp(/** @type {KeyboardEvent}*/ e){
       break;
 
   }
+}
+
+function newAsteroid(x, y) {
+  let roid = {
+    // x position
+    x: x,
+    // y position
+    y: y,
+    // x velocity
+    xv: Math.random() * ROIDS_SPD / FPS * (Math.random() < .5 ? 1 : -1),
+    // y velocity
+    yv: Math.random() * ROIDS_SPD / FPS * (Math.random() < .5 ? 1 : -1),
+    // size of asteroid
+    r: ROIDS_SIZE / 2,
+    // angle of asteroid movementation
+    a: Math.random() * Math.PI * 2, // in radians
+    // number of asteroid's vertices
+    vert: Math.floor(Math.random() * (ROIDS_VERT + 1) + ROIDS_VERT / 2),
+
+    offs: []
+  };
+
+  // create the vertex offsets array
+  for (let i = 0; i < roid.vert; i++) {
+    roid.offs.push(Math.random() * ROIDS_JAG * 2 + 1 - ROIDS_JAG)
+  }
+  return roid;
 }
 
 function update () {
@@ -124,6 +182,54 @@ function update () {
   );
   ctx.closePath();
   ctx.stroke();
+
+  // draw asteroids
+  ctx.strokeStyle = 'slategray';
+  ctx.lineWidth = SHIP_SIZE / 20;
+  let x, y, r, a, vert, offs;
+  for (let i = 0; i < roids.length; i++){
+    // get the asteroids properties
+    x = roids[i].x;
+    y = roids[i].y;
+    r = roids[i].r;
+    a = roids[i].a;
+    vert = roids[i].vert;
+    offs = roids[i].offs;
+    // draw a path
+    ctx.beginPath();
+    ctx.moveTo(
+      x + r * offs[0] * Math.cos(a),
+      y + r * offs[0] * Math.sin(a)
+    );
+
+    // draw a poligon
+    for (let j = 1; j < vert; j++) {
+      ctx.lineTo(
+        x + r * offs[j] * Math.cos(a +j * Math.PI * 2 / vert),
+        y + r * offs[j] * Math.sin(a +j * Math.PI * 2 / vert),
+      )
+    }
+
+    ctx.closePath();
+    ctx.stroke();
+
+    // move the asteroid
+    roids[i].x += roids[i].xv;
+    roids[i].y += roids[i].yv;
+
+    // handle edge of screen
+    if (roids[i].x < 0 - roids[i].r) {
+      roids[i].x = canva.width + roids[i].r
+    } else if (roids[i].x > canva.width + roids[i].r) {
+      roids[i].x = 0 - roids[i].r
+    }
+
+    if (roids[i].y < 0 - roids[i].r) {
+      roids[i].y = canva.height + roids[i].r
+    } else if (roids[i].y > canva.height + roids[i].r) {
+      roids[i].y = 0 - roids[i].r
+    }
+  }
 
   // rotate the ship 
   ship.a += ship.rot;
